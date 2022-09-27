835c57ee3e73ae2d0d1c892e47049ae543319096
taufiqaa <mo.taufiqaa@gmail.com>
2022-09-27 21:10:52 +0700

    updated

--- src/main/java/com/mytask/controllers/VehicleDataController.java
+++ src/main/java/com/mytask/controllers/VehicleDataController.java
@@ -14,9 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mytask.models.entities.VehicleData;
 import com.mytask.services.VehicleDataService;
 
-@CrossOrigin(origins = "https://taufiq-vehicle-data.vercel.app", methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.PATCH,RequestMethod.DELETE}
-
-)
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/vehicle-data")
 public class VehicleDataController {
